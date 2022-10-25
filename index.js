68f5841d7599c7f632870f41d8c9f68724b7ed2e
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-25 12:36:56 +0600

    courses data updated and single course data res created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = './data/courses.json'
+const courses = require("./data/courses.json");
 
 
 app.get('/', (req, res) => {
@@ -14,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/courses', (req, res) => {
    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+   const id = req.params.id;
+   const singleCourse = courses.find(course => course.id === id)
+   res.send(singleCourse);
 })
 
 app.listen(port, () => {
